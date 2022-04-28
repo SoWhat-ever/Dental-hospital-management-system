899305f0aed945a62135284026d18098f2c7a3d3
lyh <7143192@sjtu.edu.cn>
2022-04-28 12:54:14 +0800

    submit some original backend files by yhan

---
+++ DentistBackend/src/main/java/com/example/dentistbackend/controller/PatientController.java
@@ -0,0 +1,24 @@
+package com.example.dentistbackend.controller;
+import com.example.dentistbackend.entity.Patient;
+import com.example.dentistbackend.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Map;
+@RestController
+public class PatientController {
+    @Autowired
+    private PatientService PatientService;
+    @RequestMapping("/getPatients")
+    public List<Patient> getPatients(@RequestBody Map<String, String> params) {
+        return PatientService.getPatients();
+    }
+
+    @RequestMapping("/getPatient")
+    public Patient getPatient(@RequestParam("id") Integer id){
+        return PatientService.findPatientById(id);
+    }
+}
