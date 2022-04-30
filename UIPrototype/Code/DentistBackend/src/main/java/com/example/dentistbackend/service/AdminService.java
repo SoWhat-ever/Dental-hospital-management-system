74606bc27df96831ae803ca000d1a3553f2e0bd3
7143192 <90765394+7143192@users.noreply.github.com>
2022-04-30 10:05:55 +0800

    Delete UIPrototype/Code/DentistBackend directory

--- UIPrototype/Code/DentistBackend/src/main/java/com/example/dentistbackend/service/AdminService.java
+++
@@ -1,6 +0,0 @@
-package com.example.dentistbackend.service;
-import com.example.dentistbackend.entity.Admin;
-import java.util.List;
-public interface AdminService {
-    Admin checkUser(String username, String password);
-}
